c173bb51b6b34c80fb912c92fff62b3230af061b
Van Pham <111153414+vanpham1999@users.noreply.github.com>
2023-02-03 11:43:54 -0600

    Create Movies.py

---
+++ Movies.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import matplotlib
+plt.style.use('ggplot')
+from matplotlib.pyplot import figure
+
+%matplotlib inline
+matplotlib.rcParams['figure.figsize'] = (12,13)
+
+pd.options.mode.chained_assignment = None
+for col in df.columns:
+    pct_missing = np.mean(df[col].isnull())
+    print('{} - {}%'.format(col, round(pct_missing*100)))
